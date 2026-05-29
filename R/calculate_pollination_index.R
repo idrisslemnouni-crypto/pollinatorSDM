bc4f62f34d38234d3ee28df5a20ad239f0c515bf
Idriss Lemnouni <idrisslemnouni@gmail.com>
2026-05-29 19:00:17 +0100

    Integration donnees cultures dans indice pollinisation - vignette et rapport

--- R/calculate_pollination_index.R
+++ R/calculate_pollination_index.R
@@ -1,8 +1,17 @@
 #' Calculate pollination index
-#' @param raster_list List of SpatRaster (one per species)
+#' @param prediction_raster SpatRaster
+#' @param env_rasters SpatRaster
+#' @param crop_data Optional data.frame with dependence_pollination column
 #' @return SpatRaster
 #' @export
-calculate_pollination_index <- function(raster_list) {
-  s <- terra::rast(raster_list)
-  terra::mean(s, na.rm = TRUE)
+calculate_pollination_index <- function(prediction_raster, env_rasters, crop_data = NULL) {
+  base_index <- terra::app(prediction_raster, fun = function(x) mean(x, na.rm = TRUE))
+
+  if (!is.null(crop_data) && "dependence_pollination" %in% names(crop_data)) {
+    mean_dep <- mean(crop_data$dependence_pollination, na.rm = TRUE)
+    base_index <- base_index * mean_dep
+  }
+
+  names(base_index) <- "pollination_index"
+  return(base_index)
 }
