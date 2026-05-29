d19b6989cd86589a72151e0e49a59e4f896c6850
Idriss Lemnouni <idrisslemnouni@gmail.com>
2026-05-29 21:25:22 +0100

    Integration WorldClim reelle via geodata pour le Maroc

--- R/download_environmental_layers.R
+++ R/download_environmental_layers.R
@@ -1,21 +1,53 @@
 #' Download environmental layers
-#' @param variables Character vector of variables
-#' @param extent Numeric extent vector
-#' @return SpatRaster
+#' @return list of SpatRaster
 #' @export
-download_environmental_layers <- function(variables = c("temperature", "precipitation", "landcover", "elevation"), extent = c(-15, 25, 25, 55)) {
-  message("Downloading WorldClim data...")
-  n <- 100 * 100
-  rasts <- lapply(variables, function(var) {
-    vals <- switch(var,
-                   temperature = rnorm(n, 15, 10),
-                   precipitation = rgamma(n, 2, 0.01),
-                   landcover = sample(1:5, n, replace = TRUE),
-                   elevation = runif(n, 0, 3000),
-                   runif(n))
-    terra::rast(nrows = 100, ncols = 100, crs = "EPSG:4326", extent = extent, vals = vals)
-  })
-  s <- terra::rast(rasts)
-  names(s) <- variables
-  s
+download_environmental_layers <- function() {
+  if (requireNamespace("geodata", quietly = TRUE)) {
+    message("Downloading WorldClim data for Morocco (this may take 1-2 minutes)...")
+
+    tdir <- tempdir()
+
+    # WorldClim Bioclim: BIO1 = mean temp, BIO12 = annual precipitation
+    wc <- geodata::worldclim_country(country = "Morocco", var = "bio", res = 5, path = tdir)
+    temp_raster <- wc[[1]]
+    prec_raster <- wc[[12]]
+
+    # Elevation
+    alt_raster <- geodata::elevation_30s(country = "Morocco", path = tdir)
+
+    # Landcover (tree cover fraction), resampled to same grid
+    lc <- tryCatch({
+      lcov <- geodata::landcover(var = "trees", path = tdir)
+      terra::resample(lcov, temp_raster, method = "near")
+    }, error = function(e) {
+      message("Real landcover unavailable, using simulated fallback")
+      terra::rast(nrows = terra::nrow(temp_raster),
+                  ncols = terra::ncol(temp_raster),
+                  xmin = terra::xmin(temp_raster),
+                  xmax = terra::xmax(temp_raster),
+                  ymin = terra::ymin(temp_raster),
+                  ymax = terra::ymax(temp_raster),
+                  crs = terra::crs(temp_raster),
+                  vals = sample(1:5, terra::ncell(temp_raster), replace = TRUE))
+    })
+
+    names(temp_raster) <- "temperature"
+    names(prec_raster) <- "precipitation"
+    names(alt_raster) <- "altitude"
+    names(lc) <- "landcover"
+
+  } else {
+    message("Package 'geodata' not installed. Using simulated data.")
+    ext <- terra::ext(-10, 0, 28, 36)
+    temp_raster <- terra::rast(nrows = 50, ncols = 50, extent = ext, crs = "EPSG:4326", vals = rnorm(2500, 18, 5))
+    prec_raster <- terra::rast(nrows = 50, ncols = 50, extent = ext, crs = "EPSG:4326", vals = rgamma(2500, shape = 2, scale = 150))
+    alt_raster <- terra::rast(nrows = 50, ncols = 50, extent = ext, crs = "EPSG:4326", vals = runif(2500, 0, 1500))
+    lc <- terra::rast(nrows = 50, ncols = 50, extent = ext, crs = "EPSG:4326", vals = sample(1:5, 2500, replace = TRUE))
+    names(temp_raster) <- "temperature"
+    names(prec_raster) <- "precipitation"
+    names(alt_raster) <- "altitude"
+    names(lc) <- "landcover"
+  }
+
+  list(temp_raster = temp_raster, prec_raster = prec_raster, alt_raster = alt_raster, landcover = lc)
 }
